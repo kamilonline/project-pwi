748ea780b2ddf2ff1aacb2fa4a6175ee1a9204b8
wojci <wojciech.kurdybacha@gmail.com>
2021-01-12 00:02:49 +0100

    Added class Config with constructor in Config.py

---
+++ Config.py
@@ -0,0 +1,13 @@
+import json
+
+
+class Config:
+    def __init__(self):
+
+        with open("config/world-config.json", encoding="utf-8-sig") as file:
+            config_dict = json.load(file)
+            self.world_width = config_dict["world_width"]
+            self.world_height = config_dict["world_height"]
+            self.plant_growth = config_dict["growth"]
+            self.growth_percentage = config_dict["growth-percentage"]
+            self.border = config_dict["border"]
