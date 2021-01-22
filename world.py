51a2ffdb538d9f3a68c8c93ddf0fdecfbf635a56
wojci <wojciech.kurdybacha@gmail.com>
2021-01-22 13:02:34 +0100

    actually added organism generation in init of class World

--- world.py
+++ world.py
@@ -1,6 +1,7 @@
 import random
 from Config import *
 from plant import Plant
+from Organism import Organism
 
 
 class World:
@@ -11,16 +12,19 @@ class World:
         self.fields = [[list() for _ in range(self.width)] for _ in range(self.height)]
         self.growth_rate = plant_config.plant_growth
 
-        # generating random coords for plants
+        # list of all possible coords
         coords = [(i, j) for i in range(self.height) for j in range(self.width)]
-        k = int(plant_config.plant_percentage * self.width * self.height) 
+        k = int(plant_config.plant_percentage * self.width * self.height)
         plant_coords = random.sample(coords, k)
 
-        # adding plant instances to coresponding fields
+        # adding plant instances to corresponding fields
         for coords in plant_coords:
             self.fields[coords[0]][coords[1]].append(Plant(list(coords)))
 
-        
+        # generating random coords for organisms
+        k = int(organism_config.organism_percentage * self.width * self.height)
+        organisms_coords = random.sample(coords, k)
 
-        
-
+        # adding organism instances to corresponding fields
+        for coords in organisms_coords:
+            self.fields[coords[0]][coords[1]].append(Organism())
