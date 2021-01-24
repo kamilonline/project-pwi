3da3908b8aa13320e2f8f4f94fb5b5a4db6c04ba
wojci <wojciech.kurdybacha@gmail.com>
2021-01-24 21:21:07 +0100

    added Plant class attributes initialization method

--- plant.py
+++ plant.py
@@ -2,5 +2,14 @@ from config import PlantConfig
 
 
 class Plant:
+    growth = 0
+    energy = 0
+
     def __init__(self, coords: list):
         self.position = coords
+
+    @classmethod
+    def initialize_class_atributes(cls, plant_config: PlantConfig):
+        cls.growth = plant_config.plant_growth
+        cls.energy = plant_config.plant_energy
+
