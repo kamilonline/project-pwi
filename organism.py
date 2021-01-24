ceced1c564c67432c1574d12c4c5d172a68f5db0
wojci <wojciech.kurdybacha@gmail.com>
2021-01-24 16:58:16 +0100

    implemented reproduction and mutation in Organism class. Added handling dict of attributes in init of Organism class

--- organism.py
+++ organism.py
@@ -1,7 +1,9 @@
+import random
 from config import OrganismConfig
 
 
 class Organism:
+    traits_names = ("sight_distance", "speed", "energy_capacity")
     mutation_probability = 0
     budding_energy_threshold = 0
     budding_time_threshold = 0
@@ -13,9 +15,16 @@ class Organism:
     eating_loss = 0
 
     def __init__(self, *args):
-        self.sight_distance = args[0]
-        self.speed = args[1]
-        self.energy_capacity = args[2]
+        if len(args) == 1:
+            collection = args[0]
+            if isinstance(collection, dict):
+                collection = list(collection.values())
+        else:
+            collection = args
+
+        self.sight_distance = collection[0]
+        self.speed = collection[1]
+        self.energy_capacity = collection[2]
 
     @classmethod
     def initialize_class_atributes(cls, organism_config: OrganismConfig):
@@ -28,3 +37,46 @@ class Organism:
         cls.walking_loss = organism_config.walking_loss
         cls.stationary_loss = organism_config.stationary_loss
         cls.eating_loss = organism_config.eating_loss
+
+    def reproduce(self):
+        """
+        creates copy of organism and mutates it according to parameters
+        :return: instance of Organism class
+        """
+        attributes = vars(self).copy()
+        if random.random() < Organism.mutation_probability:
+            # create new mutated atributes
+            attributes = list(self.__mutate_attributes(attributes).values())
+
+        new_organism = Organism(attributes)
+
+        return new_organism
+
+    @staticmethod
+    def __mutate_attributes(attributes: dict):
+        """
+        :param attributes: dict of attribute names as keys and their values
+        :return: dict with one of the attributes mutated
+        """
+
+        traits = [i for i in attributes.keys() if i in Organism.traits_names]
+
+        # choose trait
+        trait_to_mutate = random.choice(traits)
+        change = random.random()
+        sign = random.choice([-1, 1])
+        mutated_trait = attributes[trait_to_mutate]
+
+        # change the value of trait chosen to be mutated
+        mutated_trait += sign * attributes[trait_to_mutate] * change
+
+        # change data types if needed
+        if trait_to_mutate == "sight_distance":
+            mutated_trait = int(mutated_trait)
+
+        elif trait_to_mutate == "energy_capacity":
+            mutated_trait = min(255, int(mutated_trait))
+
+        attributes[trait_to_mutate] = mutated_trait
+
+        return attributes
