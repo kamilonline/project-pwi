373452eeccffb0310e09b25b01af8a250e50ae7b
wojci <wojciech.kurdybacha@gmail.com>
2021-01-25 17:53:28 +0100

    added a value (0, 0) in Direction class as a direction pointing to the object's location

--- direction.py
+++ direction.py
@@ -3,6 +3,7 @@ from math_util import *
 
 
 class Direction(Enum):
+    here = (0, 0)
     up = (-1, 0)
     down = (1, 0)
     right = (0, 1)
@@ -12,7 +13,6 @@ class Direction(Enum):
     def generate_direction(organism_coords, target_coords):
         x_distance = abs(organism_coords[0] - target_coords[0])
         y_distance = abs(organism_coords[1] - target_coords[1])
-
         result = [sign(target_coords[0] - organism_coords[0]), sign(target_coords[1] - organism_coords[1])]
         result[ReLU(sign(x_distance - y_distance))] = 0
         return Direction(tuple(result))
