a3bde5bbe5d6745d076dcb5b54836e12afb3e10c
wojci <wojciech.kurdybacha@gmail.com>
2021-01-25 22:41:18 +0100

    added destroy method in class Entity for deleting when dead

--- entity.py
+++ entity.py
@@ -3,3 +3,11 @@ class Entity:
         self.generation = generation
         self.id = id
         self.coords = coords
+
+    def destroy(self, entities, fields):
+        entities[self.generation][self.id] = None
+
+        i = self.coords[0]
+        j = self.coords[1]
+        index = fields[i][j].index(self)
+        fields[i][j][index] = None
