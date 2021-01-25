61c6401205b645209d227a8032259a21df20dac5
wojci <wojciech.kurdybacha@gmail.com>
2021-01-25 13:12:10 +0100

    refactored the constructor of World class, added two methods to World for initialization of entities

--- world.py
+++ world.py
@@ -6,22 +6,31 @@ from organism import *
 class World:
     def __init__(self, seed: int, world_config: WorldConfig, plant_config: PlantConfig, organism_config: OrganismConfig):
         random.seed(seed)
-        # todo: refactor this constructor to be shorter and more readable
         self.width = world_config.world_width
         self.height = world_config.world_height
         self.fields = [[list() for _ in range(self.width)] for _ in range(self.height)]
 
+        # dictionaries initialization
         # {generation index : {id: entitiy instance}}
         self.plants = {0: {}}
         self.organisms = {0: {}}
-        self.growth_rate = plant_config.plant_growth
 
         # list of all possible coords
         coords = [(i, j) for i in range(self.height) for j in range(self.width)]
+
+        Plant.initialize_class_atributes(plant_config)
+        self.initialize_plants(coords, plant_config)
+
+        Organism.initialize_class_atributes(organism_config)
+        self.initialize_organisms(coords, organism_config)
+
+    def initialize_plants(self, coords: list, plant_config: PlantConfig):
+        generation = 0
+
+        # generating random coords for plants
         k = int(plant_config.plant_percentage * self.width * self.height)
         plant_coords = random.sample(coords, k)
 
-        generation = 0
         # adding plant instances to corresponding fields
         for id, coords_iter in enumerate(plant_coords):
             plant = Plant(generation,
@@ -30,15 +39,14 @@ class World:
             self.fields[coords_iter[0]][coords_iter[1]].append(plant)
             self.plants[generation][id] = plant
 
+    def initialize_organisms(self, coords: list, organism_config: OrganismConfig):
+        generation = 0
+
         # generating random coords for organisms
         k = int(organism_config.organism_percentage * self.width * self.height)
         organisms_coords = random.sample(coords, k)
 
-        # set values from config to class attributes
-        Organism.initialize_class_atributes(organism_config)
-
         # adding organism instances to corresponding fields
-
         for id, coords_iter in enumerate(organisms_coords):
             organism = Organism(generation,
                                 id,
