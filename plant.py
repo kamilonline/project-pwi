aac127661f8a8abcdab73340cf60c8c5cf8e0614
wojci <wojciech.kurdybacha@gmail.com>
2021-01-28 21:04:44 +0100

    added plant growth method

--- plant.py
+++ plant.py
@@ -5,6 +5,7 @@ from entity import Entity
 class Plant(Entity):
     growth = 0
     energy = 0
+    frame_count = 0
 
     def __init__(self, generation: int, id: int, coords: list):
         super().__init__(generation, id, coords)
@@ -14,3 +15,14 @@ class Plant(Entity):
         cls.growth = plant_config.plant_growth
         cls.energy = plant_config.plant_energy
 
+    @staticmethod
+    def grow(plants, coords):
+        new_generation = list(plants.keys())[-1] + 1
+        if new_generation in plants and plants[new_generation]:
+            new_id = list(plants[new_generation].keys())[-1] + 1
+        else:
+            new_id = 0
+            if new_generation not in plants:
+                plants[new_generation] = {}
+
+        return Plant(new_generation, new_id, coords)
